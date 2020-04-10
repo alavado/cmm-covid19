f6e1f508e2388d049045237e2f2e6f14410bd4ea
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-04-10 17:10:29 -0400

    agrega geojson chile

--- src/Mapa/Mapa.js
+++ src/Mapa/Mapa.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL, { NavigationControl, Source, Layer } from 'react-map-gl'
 import mapStyle from './mapStyle.json'
+import regiones from '../data/geojsons/regiones.json'
 import './Mapa.css'
 
 const Mapa = () => {
   const [viewport, setViewport] = useState({
     width: '100%',
-    height: 'calc(100vh - 2em)',
-    latitude: -44.24,
+    height: 'calc(100vh)',
+    latitude: -38.24,
     longitude: -70.01,
-    zoom: 4,
-    zoom: 8
+    zoom: 3,
+    minZoom: 2.5
   })
 
   return (
@@ -18,8 +19,34 @@ const Mapa = () => {
       <ReactMapGL
         {...viewport}
         mapStyle={mapStyle}
+        getCursor={() => 'default'}
         onViewportChange={setViewport}
-      />
+      >
+        <Source id="capa-datos-movilidad" type="geojson" data={regiones}>
+          <Layer
+            id="data"
+            type="fill"
+            paint={{
+              'fill-color': {
+                stops: [
+                  [0, '#B0BEC5'],
+                  [.01, '#abdda4'],
+                  [.3, '#e6f598'],
+                  [.45, '#ffffbf'],
+                  [.6, '#fee08b'],
+                  [.75, '#fdae61'],
+                  [.9, '#f46d43'],
+                  [1.05, '#d53e4f']
+                ]
+              },
+              'fill-opacity': .5,
+              'fill-color-transition': {
+                'duration': 300,
+                'delay': 0
+              }
+            }}/>
+        </Source>
+      </ReactMapGL>
     </div>
   )
 }
