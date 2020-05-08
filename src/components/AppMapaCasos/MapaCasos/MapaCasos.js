60945ccfa260298e01ab1c7bf8b2b6d29ae6ea6f
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-05-08 00:32:51 -0400

    agrega puntitos

--- src/components/AppMapaCasos/MapaCasos/MapaCasos.js
+++ src/components/AppMapaCasos/MapaCasos/MapaCasos.js
@@ -1,10 +1,11 @@
-import React, { useState, useMemo } from 'react'
+import React, { useState, useMemo, useRef, useEffect } from 'react'
 import ReactMapGL, { Source, Layer, Marker } from 'react-map-gl'
 import mapStyle from './mapStyle.json'
 import './MapaCasos.css'
 import { useSelector } from 'react-redux'
 import { CASOS_COMUNALES } from '../../../redux/reducers/series'
 import polylabel from 'polylabel'
+import marcador from './marker-icon.png'
 
 const calcularPoloDeInaccesibilidad = ({ coordinates: puntos }) => {
   const [longitude, latitude] = polylabel(puntos)
@@ -15,6 +16,7 @@ const MapaCasos = () => {
 
   const { series } = useSelector(state => state.series)
   const { geoJSON } = series.find(({ id }) => id === CASOS_COMUNALES)
+  const mapCasos = useRef()
 
   const geoJSONFiltrado = useMemo(() => {
     const featuresRegion = geoJSON
@@ -44,6 +46,19 @@ const MapaCasos = () => {
     altitude: 1.5,
   })
 
+  const nombresComunas = useMemo(() => geoJSONFiltrado.features.map((feature, i) => (
+    <Marker
+      className="MapaCasos__marcador_nombre_comuna"
+      latitude={feature.properties.centro.latitude}
+      longitude={feature.properties.centro.longitude}
+      key={`marker-feature-${i}`}
+    >
+      <div className="MapaCasos__marcador_nombre_comuna_contenido">
+        {feature.properties.NOM_COM}
+      </div>
+    </Marker>
+  )), [])
+
   const cambioEnElViewport = vp => {
     setViewport(prev => ({
       ...prev,
@@ -53,18 +68,44 @@ const MapaCasos = () => {
     }))
   }
 
-  const marcadores = useMemo(() => Array.from(Array(1000).keys()).map(n => (
-    <Marker
-      className="MapaCasos__marcador_nombre_comuna"
-      latitude={-33 + Math.random() * 5}
-      longitude={-70 + Math.random() * 5}
-      key={`marker-feature-${n + 1000}`}
-    >
-      <div className="MapaCasos__marcador_caso">
+  // const marcadores = useMemo(() => Array.from(Array(1000).keys()).map(n => (
+  //   <Marker
+  //     className="MapaCasos__marcador_nombre_comuna"
+  //     latitude={-34 + Math.random() * 1}
+  //     longitude={-71 + Math.random() * 1}
+  //     key={`marker-feature-${n + Math.random()}`}
+  //   >
+  //     <div className="MapaCasos__marcador_caso">
         
-      </div>
-    </Marker>
-  )), [])
+  //     </div>
+  //   </Marker>
+  // )), [viewport])
+  
+  useEffect(() => mapCasos.current.getMap()
+  .loadImage(marcador, (err, image) => {
+    mapCasos.current.getMap().addImage('marcador', image)
+  }), [])
+
+  const generarGeoJSON = () => {
+    return {
+      'type': 'geojson',
+      'data': {
+        'type': 'FeatureCollection',
+        'features': Array.from(Array(10000).keys()).map(n => (
+          {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Point',
+              'coordinates': [
+                -71 + Math.random() * 1,
+                -34 + Math.random() * 1
+              ]
+            }
+          }
+        ))
+      }
+    }
+  }
 
   return (
     <div className="MapaCasos">
@@ -72,20 +113,9 @@ const MapaCasos = () => {
         {...viewport}
         mapStyle={mapStyle}
         onViewportChange={cambioEnElViewport}
+        ref={mapCasos}
       >
-        {marcadores}
-        {geoJSONFiltrado.features.map((feature, i) => (
-          <Marker
-            className="MapaCasos__marcador_nombre_comuna"
-            latitude={feature.properties.centro.latitude}
-            longitude={feature.properties.centro.longitude}
-            key={`marker-feature-${i}`}
-          >
-            <div className="MapaCasos__marcador_nombre_comuna_contenido">
-              {feature.properties.NOM_COM}
-            </div>
-          </Marker>
-        ))}
+        {nombresComunas}
         <Source
           id="capa-datos-regiones"
           type="geojson"
@@ -96,7 +126,7 @@ const MapaCasos = () => {
             type="fill"
             paint={{
               'fill-color': 'white',
-              'fill-opacity': 1
+              'fill-opacity': .9
             }}
           />
           <Layer
@@ -106,8 +136,23 @@ const MapaCasos = () => {
               'line-color': 'rgba(0, 0, 0, 0.5)',
               'line-width': 1
             }}
-          >
-        </Layer>
+          />
+        </Source>
+        <Source
+          id="puntitos"
+          type="geojson"
+          data={generarGeoJSON().data}
+        >
+          <Layer
+            id="data-puntitos"
+            type="circle"
+            paint={{
+              "circle-color": "#ff0000",
+              "circle-radius": 1.5,
+              "circle-stroke-color": "#ff0000",
+              "circle-stroke-opacity": 1
+            }}
+          />
         </Source>
       </ReactMapGL>
     </div>
