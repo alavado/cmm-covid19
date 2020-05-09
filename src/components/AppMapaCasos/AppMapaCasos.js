d171afa2fb5b1914127214fb38bc1543c0d300f7
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-05-08 23:02:05 -0400

    agrega boton de doble pantalla

--- src/components/AppMapaCasos/AppMapaCasos.js
+++ src/components/AppMapaCasos/AppMapaCasos.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AppMapaCasos.css'
 import MapaCasos from './MapaCasos'
 
 const AppMapaCasos = () => {
+
+  const [doble, setDoble] = useState(false)
+  const [vpMapaPrincipal, setVpMapaPrincipal] = useState({})
+  const [posicionMapaPrincipal, setPosicionMapaPrincipal] = useState(30)
+
   return (
     <div className="AppMapaCasos">
-      <MapaCasos />
+      <button className="AppMapaCasos__boton_doble" onClick={() => setDoble(!doble)}>Dobble</button>
+      <MapaCasos secundario={false} setVpMapaPrincipal={setVpMapaPrincipal} setPosicionMapaPrincipal={setPosicionMapaPrincipal} />
+      {doble && <MapaCasos secundario={true} vpMapaPrincipal={vpMapaPrincipal} posicionMapaPrincipal={posicionMapaPrincipal} />}
     </div>
   )
 }
--- src/components/AppMapaCasos/MapaCasos/MapaCasos.js
+++ src/components/AppMapaCasos/MapaCasos/MapaCasos.js
@@ -13,7 +13,7 @@ const calcularPoloDeInaccesibilidad = puntos => {
   return { longitude: longitude, latitude: latitude }
 }
 
-const MapaCasos = () => {
+const MapaCasos = props => {
 
   const { series } = useSelector(state => state.series)
   const { datos: datosComunas, geoJSON } = series.find(({ id }) => id === CASOS_COMUNALES_INTERPOLADOS)
@@ -21,6 +21,8 @@ const MapaCasos = () => {
 
   const [posicion, setPosicion] = useState(30)
   const [recuperacion, setRecuperacion] = useState(14)
+  const [multiplicador, setMultiplicador] = useState(0)
+  const posicionInicial = 27
 
   const geoJSONFiltrado = useMemo(() => {
     const featuresRegion = geoJSON
@@ -50,32 +52,52 @@ const MapaCasos = () => {
     altitude: 1.5,
   })
 
-  const nombresComunas = useMemo(() => geoJSONFiltrado.features.map((feature, i) => (
-    <Marker
+  const hashComunas = useMemo(() => datosComunas.reduce((obj, comuna) => ({
+    ...obj,
+    [comuna.codigo]: comuna
+  }), {}), [])
+
+  const nombresComunas = useMemo(() => geoJSONFiltrado.features.map((feature, i) => {
+    const serieComuna = hashComunas[feature.properties.codigo]
+    const casosComuna = serieComuna.datos[posicion].valor
+    const recuperados = (posicion - recuperacion) < posicionInicial ? 0 : serieComuna.datos[posicion - recuperacion].valor
+    return <Marker
       className="MapaCasos__marcador_nombre_comuna"
       latitude={feature.properties.centro.latitude}
       longitude={feature.properties.centro.longitude}
       key={`marker-feature-${i}`}
     >
       <div className="MapaCasos__marcador_nombre_comuna_contenido">
-        {feature.properties.NOM_COM}
+        {feature.properties.NOM_COM}<br />
+        <span style={{ fontWeight: 'bold', fontSize: '1.15em' }}>{Math.round((1 + multiplicador) * (casosComuna - recuperados))}</span>
       </div>
     </Marker>
-  )), [])
+  }), [posicion, recuperacion, multiplicador])
+
+  useEffect(() => {
+    if (props.secundario) {
+      setViewport(props.vpMapaPrincipal)
+      setPosicion(props.posicionMapaPrincipal)
+    }
+  }, [props.vpMapaPrincipal, props.posicionMapaPrincipal])
 
   const cambioEnElViewport = vp => {
-    setViewport(prev => ({
-      ...prev,
-      ...vp,
-      width: '100%',
-      height: '100vh'
-    }))
+    if (props.secundario) {
+      return
+    }
+    setViewport(prev => {
+      const nuevoVP = {
+        ...prev,
+        ...vp,
+        width: '100%',
+        height: '100vh'
+      }
+      if (!props.secundario) {
+        props.setVpMapaPrincipal(nuevoVP)
+      }
+      return nuevoVP
+    })
   }
-
-  const hashComunas = useMemo(() => datosComunas.reduce((obj, comuna) => ({
-    ...obj,
-    [comuna.codigo]: comuna
-  }), {}), [])
 
   const geoJSONInfectados = useMemo(() => {
     return {
@@ -84,23 +106,42 @@ const MapaCasos = () => {
         .map(feature => {
           const serieComuna = hashComunas[feature.properties.codigo]
           const casosComuna = serieComuna.datos[posicion].valor
-          const recuperados = posicion < recuperacion ? 0 : serieComuna.datos[posicion - recuperacion].valor
-          return casosComuna > 0 ?
-            randomPointsOnPolygon(casosComuna - recuperados, turf.polygon(feature.geometry.coordinates))
-              :
-            []
-        }).flat()
+          const recuperados = (posicion - recuperacion < posicionInicial) ? 0 : serieComuna.datos[posicion - recuperacion - posicionInicial].valor
+          return casosComuna ? randomPointsOnPolygon((1 + multiplicador) * (casosComuna - recuperados), turf.polygon(feature.geometry.coordinates)) : []
+        })
+        .flat()
     }
-  }, [posicion, recuperacion])
-
-  console.log({geoJSONInfectados})
+  }, [posicion, recuperacion, multiplicador])
 
   return (
     <div className="MapaCasos">
       <div className="MapaCasos__lateral">
-        <div>Fecha: {datosComunas[0].datos[posicion].fecha.format('DD/MM')}</div>
-        <input type="number" value={recuperacion} onChange={e => setRecuperacion(Number(e.target.value))} />
-        <input value={posicion} type="range" min={26} max={datosComunas[0].datos.length - 1} onChange={e => setPosicion(e.target.value)} />
+      {/* {!props.secundario && <h1>Simulador de "casos activos"</h1>} */}
+        <label>
+          Días de recuperación posexamen:
+          <input type="number" value={recuperacion} onChange={e => setRecuperacion(Number(e.target.value))} />
+        </label><br />
+        <label>
+          Casos no detectados por cada detección:
+          <input type="number" value={multiplicador} onChange={e => setMultiplicador(Number(e.target.value))} />
+        </label>
+        {!props.secundario &&
+          <>
+            <div>Fecha: {datosComunas[0].datos[posicion].fecha.format('DD/MM')}</div>
+            <input
+              value={posicion}
+              type="range"
+              min={posicionInicial}
+              max={datosComunas[0].datos.length - 1}
+              onChange={e => {
+                setPosicion(e.target.value)
+                if (!props.secundario) {
+                  props.setPosicionMapaPrincipal(e.target.value)
+                }
+              }}
+            />
+          </>
+        }
       </div>
       <ReactMapGL
         {...viewport}
@@ -108,7 +149,7 @@ const MapaCasos = () => {
         onViewportChange={cambioEnElViewport}
         ref={mapCasos}
       >
-        {nombresComunas}
+        {viewport.zoom > 10 && nombresComunas}
         <Source
           id="puntitos"
           type="geojson"
@@ -118,8 +159,9 @@ const MapaCasos = () => {
             id="data-puntitos"
             type="circle"
             paint={{
-              "circle-color": "#D32F2F",
-              "circle-radius": 2
+              "circle-color": "#C62828",
+              "circle-radius": 2,
+              "circle-opacity": .75
             }}
           />
         </Source>
@@ -132,7 +174,7 @@ const MapaCasos = () => {
             id="data2-poligono-stroke"
             type="line"
             paint={{
-              'line-color': 'rgba(255, 255, 255, 0.5)',
+              'line-color': 'rgba(190, 190, 190, 1)',
               'line-width': 1.5
             }}
           />
