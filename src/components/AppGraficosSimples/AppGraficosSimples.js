010b0b7c143e515a89fae48e2db71661c26dfc39
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-05-13 17:57:42 -0400

    casi se parecen

--- src/components/AppGraficosSimples/AppGraficosSimples.js
+++ src/components/AppGraficosSimples/AppGraficosSimples.js
@@ -1,29 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import './AppGraficosSimples.css'
 import { Line } from 'react-chartjs-2'
 import { useParams, useHistory, Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { CASOS_COMUNALES_INTERPOLADOS } from '../../redux/reducers/series'
 import moment from 'moment'
+import 'chartjs-plugin-annotation'
 
 const AppGraficosSimples = () => {
 
   const { series, geoJSONCuarentenas } = useSelector(state => state.series)
+  const [annotation, setAnnotation] = useState({})
   const { comuna } = useParams()
   const history = useHistory()
+
   let codigoComuna = Number(comuna)
   if (!comuna) {
     codigoComuna = 13101
   }
+
   const serieComunas = series
     .find(({ id }) => id === CASOS_COMUNALES_INTERPOLADOS)
 
   const datos = serieComunas.datos
     .find(comuna => comuna.codigo === codigoComuna)
     .datos
-    .slice(15)
 
-  const fechas = datos.map(d => d.fecha)
+  const fechas = datos.map(d => d.fecha).slice(15)
   const valores = datos.map(d => d.valor)
     .reduce((prev, v, i, arr) => {
       let sum = 0
@@ -31,26 +34,76 @@ const AppGraficosSimples = () => {
         sum += arr[j] - arr[j - 1]
       }
       return [...prev, sum / Math.min(i + 1, 7)]
-    }, [])
-  
-  let rangosCuarentenas = geoJSONCuarentenas.features
-    .map(({ properties: { Cut_Com, FInicio, FTermino } }) => ({
-      codigo: Cut_Com,
-      inicio: moment(FInicio, 'YYYY/MM/DD'),
-      fin: moment(FTermino, 'YYYY/MM/DD').add(-1, 'days')
-    }))
+    }, []).slice(15)
 
-  let cuarentenasComuna = rangosCuarentenas
-    .filter(({ codigo }) => codigo === codigoComuna)
-  
-  for (let i = 0; i < cuarentenasComuna.length - 1; i++) {
-    let j = i + 1
-    while (j < cuarentenasComuna.length - 1 && Math.abs(cuarentenasComuna[i].fin.diff(cuarentenasComuna[j].inicio, 'days')) <= 2) {
-      cuarentenasComuna[i].fin = cuarentenasComuna[j].fin.clone()
-      cuarentenasComuna[j].eliminar = true
+  const cuarentenasComuna = useMemo(() => {
+    const rangosCuarentenas = geoJSONCuarentenas.features
+      .map(({ properties: { Cut_Com, FInicio, FTermino } }) => ({
+        codigo: Cut_Com,
+        inicio: moment(FInicio, 'YYYY/MM/DD'),
+        fin: moment(FTermino, 'YYYY/MM/DD')
+      }))
+    let cuarentenas = rangosCuarentenas
+      .filter(({ codigo }) => codigo === codigoComuna)
+    
+    for (let i = 0; i < cuarentenas.length - 1; i++) {
+      let j
+      for (j = i + 1; j < cuarentenas.length && Math.abs(cuarentenas[j].inicio.diff(cuarentenas[i].fin, 'days')) <= 2; j++) {
+        cuarentenas[i].fin = cuarentenas[j].fin.clone()
+        cuarentenas[j].eliminar = true
+      }
+      i = j - 1
     }
-  }
-  cuarentenasComuna = cuarentenasComuna.filter(r => !r.eliminar)
+    return cuarentenas.filter(r => !r.eliminar)
+  }, [comuna])
+
+  useEffect(() => {
+    setAnnotation({
+      drawTime: 'afterDatasetsDraw',
+      annotations: [
+        ...cuarentenasComuna.map(cuarentena => {
+          const { inicio, fin } = cuarentena
+          let indiceInicio = fechas.findIndex(f => Math.abs(f.diff(inicio, 'days')) <= .5)
+          let indiceFin = fechas.findIndex(f => Math.abs(f.diff(fin, 'days')) <= .5)
+          if (indiceInicio < 0) {
+            indiceInicio = fechas.length - 1
+          }
+          if (indiceFin < 0) {
+            indiceFin = fechas.length - 1
+          }
+          return { i: (indiceFin + indiceInicio) / 2, dif: indiceFin - indiceInicio, indiceInicio, indiceFin }
+        }).map(({ i, dif, indiceInicio, indiceFin }) => ({
+          type: 'line',
+          mode: 'vertical',
+          scaleID: 'eje-x', 
+          borderColor: 'transparent',
+          value: i,
+          borderWidth: 2,
+          label: {
+            backgroundColor: 'transparent',
+            content: 'cuarentena',
+            fontColor: '#5E5E5E',
+            fontSize: 18 - Math.max(12 - dif, 0),
+            fontStyle: 'bold',
+            enabled: true,
+            yAdjust: -120
+          }
+      })),
+      ...fechas.map((f, i) => f.date() === 1 ? i : -1).filter(i => i >= 0).map(i => ({
+        type: 'box',
+        xScaleID: 'eje-x',
+        yScaleID: 'eje-y',
+        xMin: i - .1,
+        xMax: i + .1,
+        yMin: -valores.reduce((x, y) => Math.max(x, y)) * .03,
+        yMax: valores.reduce((x, y) => Math.max(x, y)) * .03,
+        borderWidth: 2,
+        borderColor: '#C5C5C5',
+        backgroundColor: '#C5C5C5',
+      }))
+      ]
+    })
+  }, [cuarentenasComuna, comuna])
 
   return (
     <div className="AppGraficosSimples">
@@ -89,19 +142,20 @@ const AppGraficosSimples = () => {
                 lineTension: 0,
                 borderColor: '#5E5E5E',
                 pointRadius: 0,
-                pointHitRadius: 8
+                pointHitRadius: 10
               }]
             }}
             options={{
               maintainAspectRatio: false,
               scales: {
                 xAxes: [{
+                  id: 'eje-x',
                   display: true,
                   gridLines: {
                     display: true,
                     lineWidth: fechas.map(f => cuarentenasComuna.some(({ inicio, fin }) => {
                       return (f.diff(inicio, 'days') === 0 || f.diff(fin, 'days') === 0)
-                    }) ? 4 : 1),
+                    }) ? 4 : 0),
                     borderDash: [15, 12],
                     color: '#C3C3C3'
                   },
@@ -118,33 +172,37 @@ const AppGraficosSimples = () => {
                         const { inicio, fin } = cuarentena
                         return fecha.diff(inicio, 'days') === 0 || fecha.diff(fin, 'days') === 0
                       })
-                      return fecha.date() === 1 ? fecha.format('D [de] MMMM') : (fechaEsLimiteCuarentena ? '' : null)
+                      return fecha.date() === 1 ? fecha.format('D [de] MMMM') : fechaEsLimiteCuarentena ? '' : null
                     },
                     fontColor: '#5E5E5E'
                   }
-                }],
+                }
+              ],
                 yAxes: [{
+                  id: 'eje-y',
                   gridLines: {
                     lineWidth: 0,
                     zeroLineWidth: 3.5
                   },
                   ticks: {
-                    suggestedMin: 0
+                    suggestedMin: 0,
+                    beginAtZero: true
                   }
                 }]
               },
               tooltips: {
                 callbacks: {
-                  label: item => `Nuevos casos: ${Math.round(Number(item.value))}`
+                  label: item => `${Math.round(item.value)} nuevos casos`
                 }
               },
               legend: {
                 display: false
-              }
+              },
+              annotation
             }}
           />
         </div>
-        <Link style={{ textDecoration: 'underline' }} to={`/comuna/${codigoComuna}`}>Ver esta comuna en el mapa</Link>
+        <Link style={{ textDecoration: 'underline' }} to={`/comuna/${codigoComuna}`}>Ver esta comuna en el mapa (cuidado: cambia mucho la página)</Link>
       </div>
     </div>
   )
