32834240362744d6a4a23916c438fdf4ecd9e0cf
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-05-28 19:18:32 -0400

    se parece al endcorona

--- src/components/AppMuchosGraficos/AppMuchosGraficos.js
+++ src/components/AppMuchosGraficos/AppMuchosGraficos.js
@@ -6,57 +6,107 @@ import { Line } from 'react-chartjs-2'
 const AppMuchosGraficos = () => {
 
   const { datasets } = useSelector(state => state.datasets)
-  const dataset = datasets[4]
+  const comunas = datasets[4].comunas.series.filter((x, i) => datasets[1].comunas.series[i].serie.slice(-1)[0].valor > 50)
 
-  console.log(dataset.comunas)
+  const categorias = [
+    {
+      titulo: 'Comunas con más nuevos casos que hace 7 días',
+      comunas: comunas
+        .filter(c => c.serie.slice(-1)[0].valor > c.serie.slice(-8)[0].valor)
+        .sort((c1, c2) => c1.serie.slice(-1)[0].valor > c2.serie.slice(-1)[0].valor ? -1 : 1),
+      color: '#F44336'
+    },
+    {
+      titulo: 'Comunas con menos nuevos casos que hace 7 días',
+      comunas: comunas
+        .filter(c => c.serie.slice(-1)[0].valor >= 10 && c.serie.slice(-1)[0].valor < c.serie.slice(-8)[0].valor)
+        .sort((c1, c2) => c1.serie.slice(-1)[0] < c2.serie.slice(-1)[0] ? -1 : 1),
+      color: '#FFC107'
+    },
+    {
+      titulo: 'Comunas con menos de 10 nuevos casos en los últimos 7 días',
+      comunas: comunas
+        .filter(c => c.serie.slice(-1)[0].valor < 10)
+        .sort((c1, c2) => c1.serie.slice(-1)[0] < c2.serie.slice(-1)[0] ? -1 : 1),
+      color: '#66BB6A'
+    }
+  ]
 
   return (
-    <div style={{backgroundColor: 'white', padding: '1em', overflow: 'scroll' }}>
+    <div className="AppMuchosGraficos">
       <h1>Todos los gráficos</h1>
-      <div style={{ display: 'flex', flexWrap: 'wrap', backgroundColor: 'white' }}>
-        {datasets[4].comunas.series.map(c => {
-          return (
-            <div key={c.codigo} style={{ width: '200px', height: '100px', margin: '1em', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <h1>{c.nombre}</h1>
-              <Line
-                data={{
-                  labels: c.serie.map(v => v.fecha),
-                  datasets: [{
-                    data: c.serie.map(v => v.valor),
-                    fill: false,
-                    pointRadius: 0,
-                    borderColor: 'pink'
-                  }]
-                }}
-                options={{
-                  maintainAspectRatio: false,
-                  legend: {
-                    display: false
-                  },
-                  scales: {
-                    xAxes: [{
-                      gridLines: {
+      {categorias.map(({ titulo, comunas, color }) => (
+        <div className="AppMuchosGraficos__contenedor_categoria" key={titulo}>
+          <h2 className="AppMuchosGraficos__titulo_categoria">{titulo}</h2>
+          <div className="AppMuchosGraficos__contenedor_graficos">
+            {comunas.map(c => {
+              const maximoComuna = c.serie.reduce((x, y) => Math.max(x, y.valor), 0)
+              return (
+                <div
+                  key={`grafico-${c.codigo}`}
+                  className="AppMuchosGraficos__contenedor_grafico"
+                >
+                  <Line
+                    data={{
+                      labels: c.serie.map(v => v.fecha),
+                      datasets: [{
+                        data: c.serie.map(v => v.valor + Math.max(1, maximoComuna / 10)),
+                        fill: false,
+                        pointRadius: 0,
+                        borderColor: color,
+                        borderWidth: 1.75,
+                        pointHoverRadius: 0
+                      }]
+                    }}
+                    options={{
+                      maintainAspectRatio: false,
+                      animation: false,
+                      legend: {
                         display: false
                       },
-                      ticks: {
-                        display: false
-                      }
-                    }],
-                    yAxes: [{
-                      gridLines: {
-                        display: false
+                      tooltips: {
+                        display: false,
+                        enabled: false
                       },
-                      ticks: {
-                        suggestedMax: 10
+                      scales: {
+                        xAxes: [{
+                          gridLines: {
+                            display: false
+                          },
+                          ticks: {
+                            fontColor: '#212121',
+                            maxRotation: 0,
+                            fontSize: 6,
+                            autoSkip: true,
+                            autoSkipPadding: 10,
+                            padding: -8,
+                            lineHeight: 1
+                          }
+                        }],
+                        yAxes: [{
+                          gridLines: {
+                            lineWidth: 0,
+                            zeroLineWidth: 1,
+                            zeroLineColor: '#212121'
+                          },
+                          ticks: {
+                            suggestedMax: 10,
+                            display: false
+                          },
+                          scaleLabel: {
+                            display: false
+                          }
+                        }]
                       }
-                    }]
-                  }
-                }}
-              />
-            </div>
-          )
-        })}
-      </div>
+                    }}
+                  />
+                  <h1 className="AppMuchosGraficos__nombre_comuna">{c.nombre}</h1>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
