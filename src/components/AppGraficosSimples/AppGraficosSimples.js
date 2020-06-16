42f7e47908f1112f52f84d5c9fad6243caa7bbe6
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-06-16 11:45:28 -0400

    actualiza vmi

--- src/components/AppGraficosSimples/AppGraficosSimples.js
+++ src/components/AppGraficosSimples/AppGraficosSimples.js
@@ -3,6 +3,7 @@ import './AppGraficosSimples.css'
 import { NavLink, Switch, Route } from 'react-router-dom'
 import GraficosSimples from './GraficosSimples'
 import GraficosVMI from './GraficosVMI'
+import AppMuchosGraficos from '../AppMuchosGraficos'
 
 const AppGraficosSimples = () => {
 
@@ -17,6 +18,13 @@ const AppGraficosSimples = () => {
           Nuevos casos en los últimos 7 días
         </NavLink>
         <NavLink
+          to="/graficos/todos"
+          className="AppGraficosSimples__link_navegacion"
+          activeClassName="AppGraficosSimples__link_navegacion--activo"
+        >
+          Todos los gráficos
+        </NavLink>
+        <NavLink
           to="/vmi"
           className="AppGraficosSimples__link_navegacion"
           activeClassName="AppGraficosSimples__link_navegacion--activo"
@@ -26,6 +34,7 @@ const AppGraficosSimples = () => {
       </nav>
       <div className="AppGraficosSimples__contenedor">
         <Switch>
+          <Route path="/graficos/todos" component={AppMuchosGraficos} />
           <Route path="/graficos/comuna/:comuna" component={GraficosSimples} />
           <Route path="/graficos" component={GraficosSimples} />
           <Route path="/vmi" component={GraficosVMI} />
--- src/components/AppMuchosGraficos/AppMuchosGraficos.js
+++ src/components/AppMuchosGraficos/AppMuchosGraficos.js
@@ -12,7 +12,7 @@ const AppMuchosGraficos = () => {
 
   const categorias = [
     {
-      titulo: 'Comunas con muchos nuevos casos en los últimos 7 días',
+      titulo: 'Comunas con aumento de casos en los últimos 7 días',
       explicacion: 'Más nuevos casos en los últimos 7 días que en los 7 días anteriores',
       comunas: comunas
         .filter(c => c.serie.slice(-1)[0].valor >= 10 && c.serie.slice(-1)[0].valor > c.serie.slice(-8)[0].valor)
@@ -40,13 +40,14 @@ const AppMuchosGraficos = () => {
   return (
     <div className="AppMuchosGraficos">
       <h1 className="AppMuchosGraficos__titulo">Todos los gráficos de comunas con más de 50 casos</h1>
-      <p>De acuerdo al último informe epidemiológico, publicado el {comunas[0].serie.slice(-1)[0].fecha}</p>
-      <p>Inspirado en <a href="https://www.endcoronavirus.org/countries#action" target="_blank" rel="noopener noreferrer">EndCoronaVirus.org</a></p>
+      <p>De acuerdo con el último informe epidemiológico del {comunas[0].serie.slice(-1)[0].fecha}</p>
+      <p className="AppMuchosGraficos__inspiracion">
+        Inspirado en <a href="https://www.endcoronavirus.org/countries#action" target="_blank" rel="noopener noreferrer">EndCoronaVirus.org</a>
+      </p>
       {categorias.map(({ titulo, explicacion, comunas, color }) => (
         <div className="AppMuchosGraficos__contenedor_categoria" key={titulo}>
           <h2 className="AppMuchosGraficos__titulo_categoria">
             <span title={explicacion}>{titulo}</span>
-            <span>Ordenadas de acuerdo al número de nuevos casos en los últimos 7 días</span>
           </h2>
           <div className="AppMuchosGraficos__contenedor_graficos">
             {comunas.map(c => {
