f3d99424c7fd05089fd21c6aacb4b53346cbcbf9
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-07-31 20:53:37 -0400

    fix re ingresos

--- src/helpers/cuarentenas.js
+++ src/helpers/cuarentenas.js
@@ -14,16 +14,14 @@ export const procesarCuarentenas = geoJSON => {
 }
 
 export const obtenerCuarentenasActivas = (geoJSONCuarentenas, fecha) => {
-  // console.log(geoJSONCuarentenas)
   const f = moment(fecha, 'DD/MM')
   const c = {
     type: 'FeatureCollection',
     features: geoJSONCuarentenas.features.filter(feature => {
       const fechaInicio = moment(feature.properties.FInicio, 'YYYY/MM/DD hh:mm:ss')
-      const fechaFinal = moment(feature.properties.FTermino, 'YYYY/MM/DD hh:mm:ss')
+      const fechaFinal = feature.properties.FTermino !== null ? moment(feature.properties.FTermino, 'YYYY/MM/DD hh:mm:ss') : moment()
       return f.diff(fechaInicio, 'days') >= 0 && f.diff(fechaFinal, 'days') < 0
     })
   }
-  // console.log({c})
   return c
 }
